e89ae5eb9857dc3e1ba466843fff19cf20e97b6a
Robinson Dias <75645530+robinson-1985@users.noreply.github.com>
2023-06-24 14:18:29 +0000

    Modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home(){
-    return <h1>Seja bem vindo(a)!❤️</h1>
+    return <h1>Priscila, eu te amo. Se você me ama, dá uma risadinha! <a href='https://emojitool.com/pt/smiling-face-with-heart-shaped-eyes'>😍</a></h1>
+            
+
 }
 
 export default Home;
