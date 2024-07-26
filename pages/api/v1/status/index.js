3ae6e0f56b8602a3b38988e90718e102f2f68df2
robinson <robinpaulista85@gmail.com>
2024-07-26 19:55:43 -0300

    uploading the fixed `index.js` code

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,9 +3,8 @@ import database from "/infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString();
 
-  const databaseVersionResult = await database.query('SHOW server_version;');
-  console.log(databaseVersionResult.rows[0].server_version);
-
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
   const databaseMaxConnectionsResult = await database.query(
     'SHOW max_connections;'
   );
@@ -23,7 +22,7 @@ async function status(request, response) {
    updated_at: updatedAt,
    dependencies: {
      database: {
-        version: databaseVersionResult,
+        version: databaseVersionValue,
         max_connections: parseInt  (databaseMaxConnectionsValue),
         opened_connections: databaseOpenedConnectionsValue
      },
