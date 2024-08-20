d682ddd2b2ce54a97b3409ad66966b8843637210
Andrei Alves <138537998+andrei-alves@users.noreply.github.com>
2024-08-20 20:34:57 +0000

    iguala repositório ao do Filipe na aula `Banco de Produção no Neon (gratuito)`, exceto `pages/index.js` e `README`

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # clone-tabnews
 
-Implementação do https://www.tabnews.com.br para o https://curso.dev
+Esse é um projeto para treinar programação.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-  return <h1>Renata, eu amo você. Se você me ama, dá uma risadinha! 😎</h1>;
+  return <h1>Mãezinha, saudades eterna ❤️</h1>;
 }
 
 export default Home;
