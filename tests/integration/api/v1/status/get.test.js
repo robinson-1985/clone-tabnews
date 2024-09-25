d83417038a971432ac4df0b9c4fb7a908fc757b6
robinson <robinpaulista85@gmail.com>
2024-09-25 20:47:04 -0300

    refactor: use `orchestrator.cleanDatabase()` in tests

--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -6,7 +6,6 @@ beforeAll(async () => {
 
 describe("GET to /api/v1/status", () => {
   describe("Anonymous user", () => {
-
     test("Retrieving current system status", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status");
       expect(response.status).toBe(200);
